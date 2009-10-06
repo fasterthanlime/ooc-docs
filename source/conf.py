e15a03d5f48edeb1f949cc83a6a2ec5cddfcdf34
Amos Wenger <amoswenger@gmail.com>
2009-10-06 23:25:41 +0000

    Fixed copyright.

--- source/conf.py
+++ source/conf.py
@@ -38,7 +38,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'ooc'
-copyright = u'2009, Amos Wenger & the ooc crew'
+copyright = u'2009, nddrylliog & the ooc crew'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
